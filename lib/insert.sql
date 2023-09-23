0e17bca2b61af32c2af4d43a757b204b9ff3086e
Mercy <mercywmuriithi.mm@gmail.com>
2023-09-23 11:33:51 +0300

    lab done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr.Chocolate', 3, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy', 5, 'F', 'Grey', 'Calm', 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha', 7, 'F', 'Black', 'Harsh', 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown', 10, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa', 13, 'F', 'Brown', 'Calm', 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch', 4, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy', 9, 'F', 'White', 'Fierce', 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, NULL, NULL, NULL, NULL, 1 );
